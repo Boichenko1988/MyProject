0df3e80535f946c863b733b6d5c5a2b812f98338
Boichenko Sergei <boychenkosergeya@gmail.com>
2023-09-10 16:32:06 +0300

    Commit

--- Program_BACKUP_1576.cs
+++ Program_BACKUP_1576.cs
@@ -1,8 +1,8 @@
 ﻿
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+<<<<<<< HEAD
 
-
-
-
+=======
+>>>>>>> boichenko_2
 Console.WriteLine("Hello, World!");
--- Program_LOCAL_1576.cs
+++ Program_LOCAL_1576.cs
@@ -2,7 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 
-
-
-
 Console.WriteLine("Hello, World!");
